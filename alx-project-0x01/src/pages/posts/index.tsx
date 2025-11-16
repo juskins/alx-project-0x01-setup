5f41b3d5fdef73141dfaa7d3626668995441f7f0
juskins <omojuwababatunde1@gmail.com>
2025-11-16 23:03:23 +0100

    first commit

--- alx-project-0x01/src/pages/posts/index.tsx
+++ alx-project-0x01/src/pages/posts/index.tsx
@@ -1,8 +1,10 @@
+import Header from '@/components/layout/Header';
 import React from 'react';
 
 const Posts: React.FC = () => {
    return (
       <div className="container mx-auto p-4">
+         <Header />
          <h1 className="text-4xl font-bold mb-6">Posts</h1>
          {/* Posts will be listed here */}
       </div>
--- alx-project-0x01/src/pages/users/index.tsx
+++ alx-project-0x01/src/pages/users/index.tsx
@@ -1,8 +1,10 @@
+import Header from '@/components/layout/Header';
 import React from 'react';
 
 const Users: React.FC = () => {
    return (
       <div className="container mx-auto p-4">
+         <Header />
          <h1 className="text-4xl font-bold mb-6">Users</h1>
          {/* Users will be listed here */}
       </div>
